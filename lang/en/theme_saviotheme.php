245191a0a1d80df4fd0b665627d399313b4363f6
Andres Morelos <andresmorelos123@gmail.com>
2017-09-13 14:52:29 -0500

    Words changes

--- lang/en/theme_saviotheme.php
+++ lang/en/theme_saviotheme.php
@@ -147,3 +147,6 @@ $string['feedback_required']="<h2>Please, share your comments.</h2>";
 $string['feedback_error']="<h2>Upps, something went wrong. Try it again.</h2>";
 $string['feedback_close']="Close";
 $string['feedback_return']="Return to feedback form";
+
+
+$string['gotocourse'] = 'Go to course';
