74339a288e7f97f06ddf41ef6248777a2d00dd94
Andrés Morelos <andresmorelos123@gmail.com>
2017-11-22 14:09:27 -0500

    Cambio version.php

--- version.php
+++ version.php
@@ -6,5 +6,5 @@ $plugin->version   = 2017091300;
 $plugin->requires  = 2016120505;
 $plugin->component = 'theme_saviotheme';
 $plugin->dependencies = array(
-    'theme_bootstrapbase'  => 2016120500,
+    'theme_bootstrapbase'  => 2014050800,
 );
